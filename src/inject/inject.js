143a905a98d298fee55fb48a2acda1e61ae2dd8e
Eugene Cheung <ey.cheung@outlook.com>
2014-10-16 16:32:46 -0400

    Loading stuff

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,21 +7,44 @@
 //   }, 10);
 // });
 
+// TODO:
+// - loading bar (analyzing 211841940 files...)
+// - screen for pages that aren't documents
+// - confidence level bars in suggestions list
+// - filter list
+// - fixed filter bar?
+
+
 // Base Sherlocke sidebar
 $('#wrap').after('<div id="sherlocke">\
     <div id="sherlocke-header">Sherlocke</div>\
-    <div id="sherlocke-filters">\
-      <span id="sherlocke-filter">All suggestions</span>\
-      <ul id="sherlocke-filters-list">\
-        <li>All suggestions</li>\
-        <li>Legislation</li>\
-        <li>Cases</li>\
-        <li>Miscellaneous</li>\
-      </ul>\
+    <div id="sherlocke-content" class="hidden">\
+      <div id="sherlocke-filters">\
+        <span id="sherlocke-filter">All suggestions</span>\
+        <ul id="sherlocke-filters-list">\
+          <li>All suggestions</li>\
+          <li>Legislation</li>\
+          <li>Cases</li>\
+          <li>Miscellaneous</li>\
+        </ul>\
+      </div>\
+      <ul id="sherlocke-suggestions"></ul>\
     </div>\
-    <ul id="sherlocke-suggestions"></ul>\
+    <div id="sherlocke-loading">\
+      <h1>Crunching data...</h1>\
+      <p id="sherlocke-loading-message"></p>\
+    </div>\
     <div id="sherlocke-toggle"></div>\
   </div>');
+
+
+var i = 1;
+var crunch = setInterval(function() { $('#sherlocke-loading-message').html('Analyzed ' + i + ' documents'); i++; }, 10);
+setTimeout(function() {
+  clearInterval(crunch);
+  $('#sherlocke-content').removeClass('hidden');
+  $('#sherlocke-loading').addClass('hidden');
+}, 2000);
 
 
 // Handle sidebar toggle
