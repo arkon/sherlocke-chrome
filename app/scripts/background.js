2547a9d0a7e2487cedb915c84414b58311ca5f37
Spencer Elliott <me@elliottsj.com>
2014-10-22 17:19:07 -0400

    Merge files

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,4 +6,29 @@ chrome.runtime.onInstalled.addListener(function (details) {
 
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+//example of using a message handler from the inject scripts
+// chrome.extension.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//   	chrome.pageAction.show(sender.tab.id);
+//     sendResponse();
+//   });
+
+
+// Create the context menu item
+chrome.contextMenus.create({
+  title: "Sherlocke: Prioritize this",
+  contexts: ["selection", "link", "editable"],
+  onclick: menuItemClicked
+});
+
+function menuItemClicked(info, tab) {
+  // launchPopup(function (newWindow) {
+  //   // called once newWindow is created
+  //   setTimeout(function () {
+  //     chrome.tabs.sendMessage(newWindow.tabs[0].id, {
+  //       type: "selectionText",
+  //       text: info.selectionText || info.linkUrl
+  //     });
+  //   }, 200);
+  // });
+}
