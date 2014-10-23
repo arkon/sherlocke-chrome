b00a79ff00adb15b5c09a1699447e113c24c0be7
Eugene Cheung <ey.cheung@outlook.com>
2014-10-23 15:55:41 -0400

    Forget that popup crap. Also, it doesn't inject into non-HTTPS pages for some reason

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,137 +1,137 @@
 'use strict';
 
-// Event listner for clicks on links in a browser action popup.
-// Open the link in a new tab of the current window.
-function onAnchorClick(event) {
- // chrome.tabs.create({
- //   selected: true,
- //   url: event.srcElement.href
- // });
+// // Event listner for clicks on links in a browser action popup.
+// // Open the link in a new tab of the current window.
+// function onAnchorClick(event) {
+//  // chrome.tabs.create({
+//  //   selected: true,
+//  //   url: event.srcElement.href
+//  // });
 
- var none = document.getElementById('priority-list-none');
+//  var none = document.getElementById('priority-list-none');
 
- if (none) {
-   none.parentElement.removeChild(none);
- }
+//  if (none) {
+//    none.parentElement.removeChild(none);
+//  }
 
- var newItem = '<li>' + event.srcElement.href + '</li>';
+//  var newItem = '<li>' + event.srcElement.href + '</li>';
 
- var inList = document.getElementById('priority-list').innerHTML.indexOf(newItem) > -1;
+//  var inList = document.getElementById('priority-list').innerHTML.indexOf(newItem) > -1;
 
- if (!inList) {
-   document.getElementById('priority-list').innerHTML += newItem;
+//  if (!inList) {
+//    document.getElementById('priority-list').innerHTML += newItem;
 
-   chrome.storage.sync.set({ 'priority-list': document.getElementById('priority-list').innerHTML });
- }
+//    chrome.storage.sync.set({ 'priority-list': document.getElementById('priority-list').innerHTML });
+//  }
 
- return false;
-}
+//  return false;
+// }
 
-// Given an array of URLs, build a DOM list of those URLs in the
-// browser action popup.
-function buildPopupDom(divName, data) {
- var ul = document.getElementById(divName);
+// // Given an array of URLs, build a DOM list of those URLs in the
+// // browser action popup.
+// function buildPopupDom(divName, data) {
+//  var ul = document.getElementById(divName);
 
- for (var i = 0, ie = data.length; i < ie; ++i) {
-   var a = document.createElement('a');
-   a.href = data[i];
-   a.appendChild(document.createTextNode(data[i]));
-   a.addEventListener('click', onAnchorClick);
+//  for (var i = 0, ie = data.length; i < ie; ++i) {
+//    var a = document.createElement('a');
+//    a.href = data[i];
+//    a.appendChild(document.createTextNode(data[i]));
+//    a.addEventListener('click', onAnchorClick);
 
-   var li = document.createElement('li');
-   li.appendChild(a);
+//    var li = document.createElement('li');
+//    li.appendChild(a);
 
-   ul.appendChild(li);
- }
-}
+//    ul.appendChild(li);
+//  }
+// }
 
-// Search history to find up to ten links that a user has typed in,
-// and show those links in a popup.
-function buildTypedUrlList(divName) {
- // To look for history items visited in the last day,
- // subtract a week of microseconds from the current time.
- var microsecondsPerDay = 1000 * 60 * 60 * 24;
- var oneDayAgo = new Date().getTime() - microsecondsPerDay;
+// // Search history to find up to ten links that a user has typed in,
+// // and show those links in a popup.
+// function buildTypedUrlList(divName) {
+//  // To look for history items visited in the last day,
+//  // subtract a week of microseconds from the current time.
+//  var microsecondsPerDay = 1000 * 60 * 60 * 24;
+//  var oneDayAgo = new Date().getTime() - microsecondsPerDay;
 
- // Track the number of callbacks from chrome.history.getVisits()
- // that we expect to get.  When it reaches zero, we have all results.
- var numRequestsOutstanding = 0;
+//  // Track the number of callbacks from chrome.history.getVisits()
+//  // that we expect to get.  When it reaches zero, we have all results.
+//  var numRequestsOutstanding = 0;
 
- chrome.history.search({
-     'text': '',              // Return every history item....
-     'startTime': oneDayAgo   // that was accessed less than one day ago.
-   },
-   function(historyItems) {
-     // For each history item, get details on all visits.
-     for (var i = 0; i < historyItems.length; ++i) {
-       var url = historyItems[i].url;
-       chrome.history.getVisits({url: url}, function(visitItems) {
-         processVisits(url, visitItems);
-       });
-       numRequestsOutstanding++;
-     }
-     if (!numRequestsOutstanding) {
-       onAllVisitsProcessed();
-     }
-   });
+//  chrome.history.search({
+//      'text': '',              // Return every history item....
+//      'startTime': oneDayAgo   // that was accessed less than one day ago.
+//    },
+//    function(historyItems) {
+//      // For each history item, get details on all visits.
+//      for (var i = 0; i < historyItems.length; ++i) {
+//        var url = historyItems[i].url;
+//        chrome.history.getVisits({url: url}, function(visitItems) {
+//          processVisits(url, visitItems);
+//        });
+//        numRequestsOutstanding++;
+//      }
+//      if (!numRequestsOutstanding) {
+//        onAllVisitsProcessed();
+//      }
+//    });
 
 
- // Maps URLs to a count of the number of times the user typed that URL into
- // the omnibox.
- var urlToCount = {};
+//  // Maps URLs to a count of the number of times the user typed that URL into
+//  // the omnibox.
+//  var urlToCount = {};
 
- // Callback for chrome.history.getVisits().  Counts the number of
- // times a user visited a URL by typing the address.
- var processVisits = function(url, visitItems) {
-   for (var i = 0, ie = visitItems.length; i < ie; ++i) {
-     // Ignore items unless the user typed the URL.
-     // if (visitItems[i].transition != 'typed') {
-     //   continue;
-     // }
+//  // Callback for chrome.history.getVisits().  Counts the number of
+//  // times a user visited a URL by typing the address.
+//  var processVisits = function(url, visitItems) {
+//    for (var i = 0, ie = visitItems.length; i < ie; ++i) {
+//      // Ignore items unless the user typed the URL.
+//      // if (visitItems[i].transition != 'typed') {
+//      //   continue;
+//      // }
 
-     if (!urlToCount[url]) {
-       urlToCount[url] = 0;
-     }
+//      if (!urlToCount[url]) {
+//        urlToCount[url] = 0;
+//      }
 
-     urlToCount[url]++;
-   }
+//      urlToCount[url]++;
+//    }
 
-   // If this is the final outstanding call to processVisits(),
-   // then we have the final results.  Use them to build the list
-   // of URLs to show in the popup.
-   if (!--numRequestsOutstanding) {
-     onAllVisitsProcessed();
-   }
- };
+//    // If this is the final outstanding call to processVisits(),
+//    // then we have the final results.  Use them to build the list
+//    // of URLs to show in the popup.
+//    if (!--numRequestsOutstanding) {
+//      onAllVisitsProcessed();
+//    }
+//  };
 
- // This function is called when we have the final list of URls to display.
- var onAllVisitsProcessed = function() {
-   // Get the top scorring urls.
-   var urlArray = [];
-   for (var url in urlToCount) {
-     urlArray.push(url);
-   }
+//  // This function is called when we have the final list of URls to display.
+//  var onAllVisitsProcessed = function() {
+//    // Get the top scorring urls.
+//    var urlArray = [];
+//    for (var url in urlToCount) {
+//      urlArray.push(url);
+//    }
 
-   // Sort the URLs by the number of times the user typed them.
-   urlArray.sort(function(a, b) {
-     return urlToCount[b] - urlToCount[a];
-   });
+//    // Sort the URLs by the number of times the user typed them.
+//    urlArray.sort(function(a, b) {
+//      return urlToCount[b] - urlToCount[a];
+//    });
 
-   buildPopupDom(divName, urlArray.slice(0, 10));
- };
-}
+//    buildPopupDom(divName, urlArray.slice(0, 10));
+//  };
+// }
 
-document.addEventListener('DOMContentLoaded', function () {
-  buildTypedUrlList("priority-history");
+// document.addEventListener('DOMContentLoaded', function () {
+//   buildTypedUrlList("priority-history");
 
-  chrome.storage.sync.get(['priority-list'], function(items) {
-    if (items['priority-list']) {
-      document.getElementById('priority-list').innerHTML = items['priority-list'];
-    }
-  });
-});
+//   chrome.storage.sync.get(['priority-list'], function(items) {
+//     if (items['priority-list']) {
+//       document.getElementById('priority-list').innerHTML = items['priority-list'];
+//     }
+//   });
+// });
 
-document.getElementById('priority-clear').addEventListener('click', function() {
-  document.getElementById('priority-list').innerHTML = '';
-   chrome.storage.sync.set({ 'priority-list': null });
-}, false);
+// document.getElementById('priority-clear').addEventListener('click', function() {
+//   document.getElementById('priority-list').innerHTML = '';
+//    chrome.storage.sync.set({ 'priority-list': null });
+// }, false);
