3bf406bc53d73bb8911f168404379b694585fd66
Eugene Cheung <ey.cheung@outlook.com>
2014-10-23 00:08:57 -0400

    Some sidebar JS code shifting around, and some Yavascript shortcuts

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,6 +1,33 @@
 'use strict';
 /* jshint multistr: true */
 
+/*
+  TODO:
+  - confidence level bars in suggestions list
+  - fixed filter bar?
+*/
+
+
+/*
+ * Some shortcuts for long JavaScript calls
+ */
+
+function getElemId(elementId) {
+  return document.getElementById(elementId);
+}
+
+Element.prototype.addClass = function(className) {
+  this.classList.add(className);
+}
+
+Element.prototype.removeClass = function(className) {
+  this.classList.remove(className);
+}
+
+Element.prototype.hasClass = function(className) {
+  return this.classList.contains(className);
+}
+
 
 /* Declare AngularJS app and modules */
 angular.module('Sherlocke', [/*'BakerStreet'*/]);
@@ -26,127 +53,96 @@ angular
  * https://docs.angularjs.org/guide/bootstrap
  */
 angular.element(document).ready(function () {
-  // Inject a side panel after CanLII's #wrap div
-  angular.element('#wrap').after('<div side-panel id="sherlocke"></div>');
 
-  angular.bootstrap(document, ['Sherlocke']);
-});
+  var optShowMenu;
+  var isDocument = false;
 
+  if (getElemId('documentHeader') !== null) {
+    isDocument = true;
+    document.body.addClass('sherlocke');
+  }
 
-// chrome.extension.sendMessage({}, function(response) {
-//   var readyStateCheckInterval = setInterval(function() {
-//   if (document.readyState === "complete") {
-//     clearInterval(readyStateCheckInterval);
+  if (isDocument) {
+    // Inject a side panel after CanLII's #wrap div
+    angular.element('#wrap').after('<div side-panel id="sherlocke"></div>');
 
-//   }
-//   }, 10);
-// });
+    angular.bootstrap(document, ['Sherlocke']);
 
-// TODO:
-// - confidence level bars in suggestions list
-// - fixed filter bar?
-
-
-// Settings
-var optShowMenu;
-
-
-var isDocument = false;
-
-if (document.getElementById('documentHeader') !== null) {
-  isDocument = true;
-  document.body.classList.add('sherlocke');
-}
-
-if (isDocument) {
-
-  var i = 1;
-  var crunch = setInterval(function() { $('#sherlocke-loading-message').html('Analyzed ' + i + ' documents'); i++; }, 10);
-  setTimeout(function() {
-    clearInterval(crunch);
-    $('#sherlocke-content').removeClass('hidden');
-    $('#sherlocke-loading').addClass('hidden');
-  }, 2000);
+    // Dummy loading
+    var i = 1;
+    var crunch = setInterval(function() { $('#sherlocke-loading-message').html('Analyzed ' + i + ' documents'); i++; }, 10);
+    setTimeout(function() {
+      clearInterval(crunch);
+      $('#sherlocke-content').removeClass('hidden');
+      $('#sherlocke-loading').addClass('hidden');
+    }, 2000);
 
 
-  // Handle sidebar toggle
-  $('#sherlocke-toggle').click(function() {
-    $('body').toggleClass('hide-sidebar');
+    // Handle sidebar toggle
+    $('#sherlocke-toggle').click(function() {
+      $('body').toggleClass('hide-sidebar');
 
-    // Sync setting
-    chrome.storage.sync.set({ 'opt-hide-sidebar': $('body').hasClass('hide-sidebar') });
-  });
+      // Sync setting
+      chrome.storage.sync.set({ 'opt-hide-sidebar': $('body').hasClass('hide-sidebar') });
+    });
 
 
-  // Handle showing/hiding of filters menu
-  var $sherlocke = $('#sherlocke');
-  var $filters   = $('#sherlocke-filters');
+    // Handle showing/hiding of filters menu
+    var $sherlocke = $('#sherlocke');
+    var $filters   = $('#sherlocke-filters');
 
-  $filters.click(function(e) {
-    $sherlocke.toggleClass('show-menu');
-
-    if (optShowMenu) {
-      chrome.storage.sync.set({ 'opt-show-menu': false });
-    }
-
-    e.stopPropagation();
-  });
-
-  document.body.addEventListener('click', function() {
-    if ($sherlocke.hasClass('show-menu')) {
-      $sherlocke.removeClass('show-menu');
+    $filters.click(function(e) {
+      $sherlocke.toggleClass('show-menu');
 
       if (optShowMenu) {
         chrome.storage.sync.set({ 'opt-show-menu': false });
       }
+
+      e.stopPropagation();
+    });
+
+    document.body.addEventListener('click', function() {
+      if ($sherlocke.hasClass('show-menu')) {
+        $sherlocke.removeClass('show-menu');
+
+        if (optShowMenu) {
+          chrome.storage.sync.set({ 'opt-show-menu': false });
+        }
+      }
+    }, false);
+
+
+    // Handle the filters
+    $('#sherlocke-filters li').click(function() {
+      $('#sherlocke-filter').html(this.innerHTML);
+    });
+
+
+    // Settings
+    chrome.storage.sync.get(['opt-hide-sidebar', 'opt-show-menu'], function(items) {
+      if (items['opt-hide-sidebar']) {
+        $('body').toggleClass('hide-sidebar', items['opt-hide-sidebar']);
+      } else {
+        chrome.storage.sync.set({ 'opt-hide-sidebar': false });
+      }
+
+      if (items['opt-show-menu']) {
+        optShowMenu = items['opt-show-menu'];
+        $sherlocke.toggleClass('show-menu', optShowMenu);
+      } else {
+        optShowMenu = true;
+        chrome.storage.sync.set({ 'opt-show-menu': true });
+      }
+    });
+
+
+    // Aggregate suggestions and show it in a list
+    var $suggestions = $('#sherlocke-suggestions');
+
+    // Dummy list items
+    for (var i = 0; i < 20; i++) {
+      $suggestions.append('<li><a href="#"><h1>Wow</h1><p>' + i + '</p><p>CanLII</p></a></li>');
     }
-  }, false);
-
-
-  // Handle the filters
-  $('#sherlocke-filters li').click(function() {
-    $('#sherlocke-filter').html(this.innerHTML);
-  });
-
-
-  // Settings
-  chrome.storage.sync.get(['opt-hide-sidebar', 'opt-show-menu'], function(items) {
-    if (items['opt-hide-sidebar']) {
-      $('body').toggleClass('hide-sidebar', items['opt-hide-sidebar']);
-    } else {
-      chrome.storage.sync.set({ 'opt-hide-sidebar': false });
-    }
-
-    if (items['opt-show-menu']) {
-      optShowMenu = items['opt-show-menu'];
-      $sherlocke.toggleClass('show-menu', optShowMenu);
-    } else {
-      optShowMenu = true;
-      chrome.storage.sync.set({ 'opt-show-menu': true });
-    }
-  });
-
-
-  // Fixed filters on scroll
-  //var filtersOffset = $filters.offset().top;
-
-  $(window).scroll(function() {
-    // var scroll = $filters.scrollTop();
-
-    // console.log(scroll + ', ' + filtersOffset);
-
-    // if (scroll >= filtersOffset)
-    //   $filters.addClass('fixed');
-    // else
-    //   $filters.removeClass('fixed');
-  });
-
-
-  // Aggregate suggestions and show it in a list
-  var $suggestions = $('#sherlocke-suggestions');
-
-  for (var i = 0; i < 20; i++) {
-    $suggestions.append('<li><a href="#"><h1>Wow</h1><p>' + i + '</p><p>CanLII</p></a></li>');
   }
 
-}  // End of isDocument
+});
