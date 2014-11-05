e5d88e62f0bf7bdbc5d46227f3438b77ee0f7aee
Spencer Elliott <me@elliottsj.com>
2014-11-05 00:29:00 -0500

    Add SherlockApp angular app

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,13 +1,86 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+var BAKERSTREET_API = 'http://api.sherlocke.me/api';
+
+/* Declare AngularJS app */
+angular.module('SherlockeApp', ['DjangoAuth']);
+
+/* Eagerly instantiate services once modules are loaded */
+function run($log, SherlockeService) {
+  if (!SherlockeService) {
+    $log.warn('SherlockService not instantiated');
+  }
+}
+run.$inject = ['$log', 'SherlockeService'];
+angular
+    .module('SherlockeApp')
+    .run(run);
+
+/*
+ * Provider configuration
+ */
+function config(AuthProvider) {
+  // Configure Auth service with AuthProvider
+  AuthProvider.loginPath(BAKERSTREET_API + '/users/sign_in.json');
+  AuthProvider.logoutPath(BAKERSTREET_API + '/users/sign_out.json');
+}
+config.$inject = ['AuthProvider'];
+angular
+    .module('SherlockeApp')
+    .config(config);
+
+/*
+ * Services
+ */
+function SherlockeService($q, Auth) {
+  this.authenticate = function (email, password) {
+    return Auth.login({
+      email: email,
+      password: password
+    }).then(function (token) {
+      debugger;
+    });
+  };
+}
+SherlockeService.$inject = ['$q', 'Auth'];
+angular
+    .module('SherlockeApp')
+    .service('SherlockeService', SherlockeService);
+
+
+/*
+ * Listen for incoming messages
+ */
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (!('origin' in message) || !('type' in message)) {
+    sendResponse({error: 'Must specify \'origin\' and \'type\''});
+    return;
+  }
+
+  if (message.origin === 'options') {
+    // Message from options.html
+    if (message.type === 'authenticate') {
+      // User is logging in
+      if (!('data' in message)) {
+        sendResponse({error: 'Expected \'data\', but none provided'});
+        return;
+      }
+
+      // Get email and password
+      var email = message.data.email;
+      var password = message.data.password;
+
+      // Authenticate using SherlockeService
+      var SherlockeService = angular.element(document.body).injector().get('SherlockeService');
+      SherlockeService.authenticate(email, password);
+    }
+  } else {
+    sendResponse({error: 'Invalid origin \'' + message.origin + '\''});
+  }
 });
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  chrome.pageAction.show(sender.tab.id);
-  sendResponse();
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('previousVersion', details.previousVersion);
 });
 
 
@@ -31,5 +104,7 @@ chrome.contextMenus.create({
   onclick: menuItemClicked
 });
 
-/* Declare AngularJS app */
-angular.module('SherlockeApp', []);
+/* Bootstrap the app onto the generated background page */
+angular.element(document).ready(function() {
+  angular.bootstrap(document, ['SherlockeApp']);
+});
