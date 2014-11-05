62f26814d33ab9d060a32a6b9de25c58ce5e4798
Spencer Elliott <me@elliottsj.com>
2014-11-05 00:29:10 -0500

    Add SherlockeOptions app

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,39 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+/* Declare AngularJS app */
+angular.module('SherlockeOptions', []);
+
+/*
+ * Controllers
+ */
+function AuthController($log) {
+  var vm = this;
+
+  vm.isAuthenticated = false;
+  vm.email = '';
+  vm.password = '';
+
+  vm.authenticate = function () {
+    // Send a message to background.js with the email and password
+    chrome.runtime.sendMessage({
+      origin: 'options',
+      type: 'authenticate',
+      data: {
+        email: this.email,
+        password: this.password
+      }
+    }, function (success) {
+      if (success === undefined) {
+        // Messaging error
+        $log.error(chrome.runtime.lastError);
+      }
+
+      // `success` indicates whether authentication was successful
+      vm.isAuthenticated = success;
+    });
+  };
+}
+AuthController.$inject = ['$log'];
+angular
+    .module('SherlockeOptions')
+    .controller('AuthController', AuthController);
