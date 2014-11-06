ebe80c5223fe0d3381f05e03ad7d6bbf6aa7232e
Spencer Elliott <me@elliottsj.com>
2014-11-06 12:25:14 -0500

    Use mixin for auth header

--- app/scripts/angular-baker-street.js
+++ app/scripts/angular-baker-street.js
@@ -6,66 +6,78 @@ var BAKERSTREET_RESEARCH_API = 'http://api.sherlocke.me/api';
 /* Declare AngularJS app */
 angular.module('BakerStreet', ['restmod']);
 
-
+/* Configure providers */
 function config(restmodProvider) {
   restmodProvider.rebase({
     $config: {
-      urlPrefix: BAKERSTREET_RESEARCH_API,
-      style: 'MyStyle'
+      urlPrefix: BAKERSTREET_RESEARCH_API
     }
   });
 }
+config.$inject = ['restmodProvider'];
 angular
-  .module('BakerStreet')
-  .config(config);
+    .module('BakerStreet')
+    .config(config);
 
+/*
+ * Services
+ */
 
-function run($http) {
-  chrome.storage.sync.get(['sherlocke-token'], function (items) {
-    if ('sherlocke-token' in items) {
-      $http.defaults.headers.common.Authorization = 'Token ' + items['sherlocke-token'];
-    }
-  });
+function BakerStreetService() {
+  this.userToken = null;
+  this.getDocuments = function () {};
 }
-run.$inject = ['$http'];
 angular
-  .module('BakerStreet')
-  .run(run);
-
+    .module('BakerStreet')
+    .service('BakerStreetService', BakerStreetService);
 
 /* Define restmod models */
+function BaseModel(restmod, BakerStreetService) {
+  return restmod.mixin({
+    $hooks: {
+      'before-request': function(_req) {
+        if (BakerStreetService.userToken !== null) {
+          _req.headers = angular.extend(_req.headers, {'Authorization': 'Token ' + BakerStreetService.userToken});
+        }
+      }
+    }
+  });
+}
+BaseModel.$inject = ['restmod', 'BakerStreetService'];
+angular
+    .module('BakerStreet')
+    .factory('BaseModel', BaseModel);
 
 var ResearchSession = ['restmod', function (restmod) {
-  return restmod.model('/research_session');
+  return restmod.model('/research_session').mix('BaseModel');
 }];
 angular
-  .module('BakerStreet')
-  .factory('ResearchSession', ResearchSession);
+    .module('BakerStreet')
+    .factory('ResearchSession', ResearchSession);
 
 // POST to make a new one (name) -> response with name/id
 // POST with parameter id
 // GET: all research sessions
 
-
 var Questions = ['restmod', function (restmod) {
   return restmod.model('/questions');
 }];
 angular
-  .module('BakerStreet')
-  .factory('Questions', Questions);
+    .module('BakerStreet')
+    .factory('Questions', Questions);
 
 
 var Pages = ['restmod', function (restmod) {
   return restmod.model('/pages');
 }];
 angular
-  .module('BakerStreet')
-  .factory('Pages', Pages);
+    .module('BakerStreet')
+    .factory('Pages', Pages);
 
 
 var Documents = ['restmod', function (restmod) {
   return restmod.model('/documents');
 }];
 angular
-  .module('BakerStreet')
-  .factory('Documents', Documents);
+    .module('BakerStreet')
+    .factory('Documents', Documents);
